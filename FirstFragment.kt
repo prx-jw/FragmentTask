56d38bf7556b23b7f79cc94fa5bdb0921eda6905
Alex <prx-jw@mail.ru>
2022-12-11 23:22:08 +0300

    fragment commit

--- FirstFragment.kt
+++ FirstFragment.kt
@@ -5,15 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 
 class FirstFragment : Fragment(R.layout.fragment_first) {
+
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View? {
+//        val root = inflater.inflate(R.layout.fragment_first, container, false)
+//        return root
+//    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+         val button = view.findViewById<Button>(R.id.swithButton)
+        button.setOnClickListener {
+            (requireActivity() as MainActivity)
+        }
+    }
 
     companion object {
          // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance() =
             FirstFragment()
-            }
+
     }
 }
--- MainActivity.kt
+++ MainActivity.kt
@@ -5,11 +5,21 @@ import android.os.Bundle
 import androidx.fragment.app.FragmentManager
 
 class MainActivity : AppCompatActivity() {
+    lateinit var firstFragment: FirstFragment
+    lateinit var secondFragment: SecondFragment
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//        val activity
-        val FragmentManager = supportFragmentManager
+        firstFragment = FirstFragment.newInstance()
+        secondFragment = SecondFragment.newInstance()
+    }
 
+    fun swithToFirstFragment(){
+        supportFragmentManager
+                .beginTransaction()
+                .replace(R.id.container,firstFragment)
+                .commit()
     }
 }
